aa9c81dd7e27bf05fe41e076578e5f0772674ff5
nazarpavkovych <nazarpavkovych@Nazars-MacBook-Air.local>
2025-12-15 23:13:12 +0100

    Add client frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { ThemeProvider, CssBaseline } from '@mui/material';
-import theme from '../../theme';
+import theme from './theme';
 import HomePage from './HomePage';
 
 function App() {
